765ade7f85afe6412c488314d65a14e8b85936e5
DavidV0 <davidvelickovic@hotmail.com>
2024-02-26 16:02:48 +0100

    character und hünchen platziert

--- models/character.class.js
+++ models/character.class.js
@@ -1,9 +1,7 @@
 class Character extends MoveableObject {
-  x;
-  y;
-
+  
   constructor() {
-    super().loadImage("./img/2_character_pepe/2_walk/W-21.png");
+    super().loadImage("../img/2_character_pepe/2_walk/W-21.png");
   }
 
   jump() {}
--- models/world.class.js
+++ models/world.class.js
@@ -2,13 +2,38 @@ class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
   ctx;
+  canvas;
   constructor(canvas) {
+    this.canvas = canvas;
     // diese Variable greift auf das Canvas zu lässt drauf malen
     this.ctx = canvas.getContext("2d");
     this.draw();
   }
 
   draw() {
-    this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.height, this.character.width );
+    this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height)
+
+    this.ctx.drawImage(
+      this.character.img,
+      this.character.x,
+      this.character.y,
+      this.character.height,
+      this.character.width
+    );
+
+    this.enemies.forEach(enemy => {
+        this.ctx.drawImage(
+            enemy.img,
+            enemy.x,
+            enemy.y,
+            enemy.height,
+            enemy.width
+          );
+    })
+    //FDraw() wird immer wieder ausgeführt
+    let self = this;
+    requestAnimationFrame(function () {
+      self.draw();
+    });
   }
 }
