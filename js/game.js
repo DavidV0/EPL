1257969ad752b6de1a5aeb94849137dbd75c9f55
DavidV0 <davidvelickovic@hotmail.com>
2024-02-27 13:19:12 +0100

    keyboard

--- js/game.js
+++ js/game.js
@@ -1,17 +1,18 @@
 let canvas;
-
 let ctx;
-
 let world;
+let keyboard = new Keyboard();
 
 /**
- * 
+ *
  */
-function init(){
-    canvas = document.getElementById('canvas')
-    world = new World(canvas);
+function init() {
+  canvas = document.getElementById("canvas");
+  world = new World(canvas);
 
-
-    console.log("My Character is: ", world.character);
-    
+  console.log("My Character is: ", world.character);
 }
+
+window.addEventListener("onkeydown", (event) => {
+  console.log(event);
+});
