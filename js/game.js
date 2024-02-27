e69fd801ac0cb4b342f1c803a457b456b263b79b
DavidV0 <davidvelickovic@hotmail.com>
2024-02-27 13:20:21 +0100

    keyboard functions

--- js/game.js
+++ js/game.js
@@ -13,6 +13,44 @@ function init() {
   console.log("My Character is: ", world.character);
 }
 
-window.addEventListener("onkeydown", (event) => {
-  console.log(event);
+document.addEventListener('keydown', (e) => {
+	if (e.keyCode == 39) {
+		keyboard.RIGHT = true;
+	}
+	if (e.keyCode == 37) {
+		keyboard.LEFT = true;
+	}
+	if (e.keyCode == 38) {
+		keyboard.UP = true;
+	}
+	if (e.keyCode == 40) {
+		keyboard.DOWN = true;
+	}
+	if (e.keyCode == 32) {
+		keyboard.SPACE = true;
+	}
+	if (e.keyCode == 68) {
+		keyboard.D = true;
+	}
+});
+
+document.addEventListener('keyup', (e) => {
+	if (e.keyCode == 39) {
+		keyboard.RIGHT = false;
+	}
+	if (e.keyCode == 37) {
+		keyboard.LEFT = false;
+	}
+	if (e.keyCode == 38) {
+		keyboard.UP = false;
+	}
+	if (e.keyCode == 40) {
+		keyboard.DOWN = false;
+	}
+	if (e.keyCode == 32) {
+		keyboard.SPACE = false;
+	}
+	if (e.keyCode == 68) {
+		keyboard.D = false;
+	}
 });
