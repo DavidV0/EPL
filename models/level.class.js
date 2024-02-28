a239c1042c90d6849a6659ebae3d556f4f971a53
DavidV0 <davidvelickovic@hotmail.com>
2024-02-28 18:45:44 +0100

    level 1 created

--- models/level.class.js
+++ models/level.class.js
@@ -2,4 +2,11 @@ class Level {
   enemies;
   clouds;
   backgroundObjects;
+
+  constructor(enemies, clouds, backgroundObject) {
+    this.enemies = enemies;
+    this.clouds = clouds;
+    this.backgroundObject = backgroundObject;
+    this.coins = coins
+  }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -1,35 +1,12 @@
 class World {
   character = new Character();
-  enemies = [new Chicken(), new Chicken(), new Chicken()];
-  clouds = [new Clouds(), new Clouds(), new Clouds()];
-  backgroundObjects = [
-     new BackgroundObject("./img/5_background/layers/air.png", -719),
-    new BackgroundObject("./img/5_background/layers/3_third_layer/2.png", -719),
-    new BackgroundObject("./img/5_background/layers/2_second_layer/2.png", -719),
-    new BackgroundObject("./img/5_background/layers/1_first_layer/2.png", -719),
-
-    new BackgroundObject("./img/5_background/layers/air.png", 0),
-    new BackgroundObject("./img/5_background/layers/3_third_layer/1.png", 0),
-    new BackgroundObject("./img/5_background/layers/2_second_layer/1.png", 0),
-    new BackgroundObject("./img/5_background/layers/1_first_layer/1.png", 0),
-    new BackgroundObject("./img/5_background/layers/air.png", 719),
-    new BackgroundObject("./img/5_background/layers/3_third_layer/2.png", 719),
-    new BackgroundObject("./img/5_background/layers/2_second_layer/2.png", 719),
-    new BackgroundObject("./img/5_background/layers/1_first_layer/2.png", 719),
-
-    new BackgroundObject("./img/5_background/layers/air.png", 719*2),
-    new BackgroundObject("./img/5_background/layers/3_third_layer/1.png", 719*2),
-    new BackgroundObject("./img/5_background/layers/2_second_layer/1.png", 719*2),
-    new BackgroundObject("./img/5_background/layers/1_first_layer/1.png", 719*2),
-    new BackgroundObject("./img/5_background/layers/air.png", 719*3),
-    new BackgroundObject("./img/5_background/layers/3_third_layer/2.png", 719*3),
-    new BackgroundObject("./img/5_background/layers/2_second_layer/2.png", 719*3),
-    new BackgroundObject("./img/5_background/layers/1_first_layer/2.png", 719*3),
-  ];
+  enemies = level1.enemies;
+  clouds = level1.clouds;
+  backgroundObjects = level1.backgroundObject;
   ctx;
   canvas;
   keyboard;
-  camera_x = 0
+  camera_x = 0;
   constructor(canvas, keyboard) {
     this.canvas = canvas;
     // diese Variable greift auf das Canvas zu lässt drauf malen
@@ -41,8 +18,8 @@ class World {
   /**
    * config our character with all the world objects
    */
-  setWorld(){
-    this.character.world = this
+  setWorld() {
+    this.character.world = this;
   }
 
   /**
@@ -62,29 +39,27 @@ class World {
    * @param {obj} obj the image you wanna place into the canvas
    */
   addToMap(obj) {
-
-    if(obj.otherDirection){
-      this.flipImage(obj)
+    if (obj.otherDirection) {
+      this.flipImage(obj);
     }
 
     this.ctx.drawImage(obj.img, obj.x, obj.y, obj.width, obj.height);
 
-    if(obj.otherDirection){
-        this.flipImageBack(obj)
+    if (obj.otherDirection) {
+      this.flipImageBack(obj);
     }
   }
 
-
-  flipImage(obj){
+  flipImage(obj) {
     this.ctx.save();
-    this.ctx.translate(obj.width, 0)
-    this.ctx.scale(-1,1)
-    obj.x = obj.x *-1
+    this.ctx.translate(obj.width, 0);
+    this.ctx.scale(-1, 1);
+    obj.x = obj.x * -1;
   }
 
-  flipImageBack(obj){
-    obj.x = obj.x *-1
-      this.ctx.restore()
+  flipImageBack(obj) {
+    obj.x = obj.x * -1;
+    this.ctx.restore();
   }
 
   /**
@@ -95,14 +70,12 @@ class World {
 
     this.ctx.translate(this.camera_x, 0);
 
-
     this.addObjectsToMap(this.backgroundObjects);
     this.addToMap(this.character);
     this.addObjectsToMap(this.enemies);
     this.addObjectsToMap(this.clouds);
 
     this.ctx.translate(-this.camera_x, 0);
-
 
     //Draw() wird immer wieder ausgeführt
     let self = this;
