13f78c7bf3a5435dd97d3a260bcf4f2eb5d7b125
DavidV0 <davidvelickovic@hotmail.com>
2024-02-29 14:13:57 +0100

    bottles added

---
+++ models/bottle.class.js
@@ -0,0 +1,10 @@
+class Bottle extends MoveableObject{
+    y = 350;
+    width = 50;
+    height = 85;
+
+    constructor(){
+        super().loadImage("./img/6_salsa_bottle/2_salsa_bottle_on_ground.png")
+        this.x = Math.random() * 1700;
+    }
+}
--- models/world.class.js
+++ models/world.class.js
@@ -130,6 +130,7 @@ class World {
     this.addObjectsToMap(this.level.enemies);
     this.addObjectsToMap(this.level.clouds);
     this.addObjectsToMap(this.level.coins);
+    this.addObjectsToMap(this.level.bottles);
     this.addObjectsToMap(this.throwableObjects);
 
     // section for static objects
