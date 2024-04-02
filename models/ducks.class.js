e782c06bf0926b7769c22591b0f7060b2a804631
DavidV0 <davidvelickovic@hotmail.com>
2024-04-02 18:58:59 +0200

    paths

--- models/ducks.class.js
+++ models/ducks.class.js
@@ -12,11 +12,11 @@ class Duck extends MoveableObject{
   };
     
     IMAGES_WALKING = [
-      "./img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
-      "./img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
-      "./img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
+      "../img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+      "../img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
+      "../img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
     ];
-    IMAGES_DEAD = ["./img/3_enemies_chicken/chicken_small/2_dead/dead.png"];
+    IMAGES_DEAD = ["../img/3_enemies_chicken/chicken_small/2_dead/dead.png"];
 
     
     constructor() {
--- models/endboss-status-bar.class.js
+++ models/endboss-status-bar.class.js
@@ -7,12 +7,12 @@ class EndBossbar extends DrawAbleObject {
     height =50;
 
     IMAGES = [
-        './img/7_statusbars/2_statusbar_endboss/orange/orange0.png',
-        './img/7_statusbars/2_statusbar_endboss/orange/orange20.png',
-        './img/7_statusbars/2_statusbar_endboss/orange/orange40.png',
-        './img/7_statusbars/2_statusbar_endboss/orange/orange60.png',
-        './img/7_statusbars/2_statusbar_endboss/orange/orange80.png',
-        './img/7_statusbars/2_statusbar_endboss/orange/orange100.png',
+        '../img/7_statusbars/2_statusbar_endboss/orange/orange0.png',
+        '../img/7_statusbars/2_statusbar_endboss/orange/orange20.png',
+        '../img/7_statusbars/2_statusbar_endboss/orange/orange40.png',
+        '../img/7_statusbars/2_statusbar_endboss/orange/orange60.png',
+        '../img/7_statusbars/2_statusbar_endboss/orange/orange80.png',
+        '../img/7_statusbars/2_statusbar_endboss/orange/orange100.png',
 
     ];
 
